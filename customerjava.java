428c7bfdc1a109ca8799cd4752b1ea22d4a61432
Embear <117084390+EmberRos@users.noreply.github.com>
2023-01-19 16:15:47 +0800

    Add files via upload

---
+++ customerjava.java
@@ -0,0 +1,17 @@
+ abstract class CUSTOMER {
+	protected String name;
+	protected double amount;
+
+	public CUSTOMER(String name, double amount) {
+		this.name = name;
+		this.amount = amount;
+	}
+	public String getName(){
+		return name;
+
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+ 	public abstract double calculateBill();
+}
